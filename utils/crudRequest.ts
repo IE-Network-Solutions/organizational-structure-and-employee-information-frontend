60be54dfe454f269b0cda389149a84517f54c6f9
Nahi002 <nahomdebele002@gmail.com>
2024-07-26 08:56:23 +0300

    crud request reusable fetcher added

---
+++ utils/crudRequest.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosRequestConfig, Method } from 'axios';
+import { BASE_URL } from './constants';
+
+interface RequestParams {
+  url: string;
+  method: Method;
+  data?: any;
+}
+
+/**
+ * Function to perform a CRUD operation by sending a request to the API
+ * @param params The request parameters including url, method, and optional data
+ * @returns The response data from the API
+ */
+export const crudRequest = async ({ url, method, data }: RequestParams) => {
+  try {
+    const config: AxiosRequestConfig = {
+      url,
+      method,
+    };
+
+    if (data) {
+      config.data = data;
+    }
+
+    const response = await axios(config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
