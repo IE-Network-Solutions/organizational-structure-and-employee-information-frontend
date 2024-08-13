0aad3acc7149d0ccca9e9cc57dad46682703e952
ahmedahmedoumer <ahmedinoumer13@gmail.com>
2024-08-13 20:59:32 +0300

    make authentication for admin using firebase

--- app/(beforeLogin)/authentication/login/page.tsx
+++ app/(beforeLogin)/authentication/login/page.tsx
@@ -1,51 +1,164 @@
-// pages/login.js
 'use client';
 import React, { useState } from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
-import { useAuthenticationStore } from '@/store/uistate/features/authentication';
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import {
+  auth,
+  googleProvider,
+  microsoftProvider,
+} from '@/utils/firebaseConfig';
 
-const LoginPage = () => {
-  const [loading, setLoading] = useState(false);
-  const { setTenantId, setToken } = useAuthenticationStore();
+import type { FormProps } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import { Microsoft } from '@/components/Icons/microsoft';
+import { Google } from '@/components/Icons/google';
 
-  const onFinish = async () => {
-    setTenantId('9fdb9540-607e-4cc5-aebf-0879400d1f69');
-    setToken('765456789opkjhc');
+type FieldType = {
+  email: string;
+  password: string;
+  remember?: string;
+};
+
+const Login: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+
+  const handleEmailPasswordSignIn: FormProps<FieldType>['onFinish'] = async (
+    values,
+  ) => {
     setLoading(true);
+    setError('');
     try {
-    } catch (error) {}
+      await signInWithEmailAndPassword(auth, values.email, values.password);
+    } catch (err: any) {
+      setError(err.message);
+    }
+    setLoading(false);
+  };
+
+  const handleGoogleSignIn = async () => {
+    setError('');
+
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  const handleMicrosoftSignIn = async () => {
+    setError('');
+
+    try {
+      await signInWithPopup(auth, microsoftProvider);
+    } catch (err: any) {
+      setError(err.message);
+    }
   };
 
   return (
-    <div style={{ maxWidth: 300, margin: '0 auto', padding: '50px 0' }}>
-      <h2>Login</h2>
-      <Form name="login" initialValues={{ remember: true }} onFinish={onFinish}>
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: 'Please input your Username!' }]}
+    <div
+      className="h-screen w-full flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat px-4"
+      style={{ backgroundImage: 'url(/login-background.png)', margin: 0 }}
+    >
+      <div className="bg-[#F1F2F3] w-full max-w-md py-4 px-6 rounded-lg my-5">
+        <p className="text-center font-semibold">PEP</p>
+        <h5 className="text-center my-2">Admin Login</h5>
+        <Form
+          name="login-form"
+          labelCol={{ span: 24 }}
+          wrapperCol={{ span: 24 }}
+          initialValues={{ remember: true }}
+          onFinish={handleEmailPasswordSignIn}
+          autoComplete="off"
         >
-          <Input placeholder="Username" />
-        </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email!',
+                type: 'email',
+              },
+            ]}
+          >
+            <Input 
+              placeholder="type your email"
+              className="w-full h-10"
+              allowClear
+            />
+          </Form.Item>
 
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
-        >
-          <Input.Password placeholder="Password" />
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password 
+                placeholder="type your email"
+                className="w-full h-10"
+                allowClear
+            />
+          </Form.Item>
 
-        <Form.Item name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox className="m-1">Remember me</Checkbox>
+            </Form.Item>
+            <Button className="float-right m-0 p-0" type="link">
+              Forgot password
+            </Button>
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" loading={loading}>
-            Log in
+          <Form.Item>
+            <Button
+              loading={loading}
+              className="py-5 my-4"
+              type="primary"
+              htmlType="submit"
+              block
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        <p className="text-center text-xs font-light mb-5">Or login with</p>
+        <div className="flex flex-col sm:flex-row gap-2">
+          <Button
+            size="small"
+            icon={<Google />}
+            className="p-4 text-sm bg-transparent"
+            onClick={handleGoogleSignIn}
+            block
+          >
+            Google
           </Button>
-        </Form.Item>
-      </Form>
+          <Button
+            size="small"
+            icon={<Microsoft />}
+            className="p-4 text-sm bg-transparent"
+            onClick={handleMicrosoftSignIn}
+            block
+          >
+            Microsoft
+          </Button>
+        </div>
+
+        {error && (
+          <p className="text-red-500 mt-5 text-center">Something happened!</p>
+        )}
+      </div>
+      <div className="text-xs font-thin text-center">
+        © 2024 PEP. All-rights reserved.
+        <span className="font-semibold ml-1 cursor-pointer">
+          Terms & Conditions
+        </span>
+        <span className="font-semibold ml-1 cursor-pointer">
+          Privacy Settings
+        </span>
+      </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default Login;
