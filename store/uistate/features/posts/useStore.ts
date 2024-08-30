dbdcdf4f089d9416374c88a5f7c12e942dc41f71
vadim-kropotov <dooondron@gmail.com>
2024-08-30 15:11:16 +0300

    lint fix

--- store/uistate/features/posts/useStore.ts
+++ store/uistate/features/posts/useStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { MyState } from './interface';
 import createCounterSlice from './createCounterSlice';
 
-const usePostStore = create<MyState>((set, get) => ({
+const usePostStore = create<MyState>((set) => ({
   ...createCounterSlice(set),
 }));
 
