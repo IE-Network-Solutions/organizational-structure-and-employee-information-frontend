cc4bee5f905f2506091429f1fb0af35182d37d3b
Ruslan Kostenko <kostenko@lobsterlab.io>
2024-09-05 13:37:17 +0700

    Add ecosystem config for process management

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'pep_front_stage',
+      script: 'npm',
+      args: 'run start',
+    },
+    {
+      name: 'pep_front_dev',
+      script: 'npm',
+      args: 'run start',
+    },
+  ],
+};
