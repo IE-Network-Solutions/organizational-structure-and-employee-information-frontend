eaaeccef0a6d13835efefe3e94aafc5ee37c12c2
vadim-kropotov <dooondron@gmail.com>
2024-09-09 17:04:26 +0300

    disable middleware

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,30 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getCookie } from '@/helpers/storageHelper';
+import { NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  try {
-    const token = getCookie('token', req);
-    const url = req.nextUrl;
-    const pathname = url.pathname;
-    const excludePath = '/authentication/login';
-    const isExcludedPath = pathname.startsWith(excludePath);
+// export function middleware(req: NextRequest) {
+//   try {
+//     const token = getCookie('token', req);
+//     const url = req.nextUrl;
+//     const pathname = url.pathname;
+//     const excludePath = '/authentication/login';
+//     const isExcludedPath = pathname.startsWith(excludePath);
+//
+//     if (!isExcludedPath && !token) {
+//       return NextResponse.redirect(new URL('/authentication/login', req.url));
+//     }
+//     if (isExcludedPath && token) {
+//       return NextResponse.redirect(
+//         new URL('/employees/manage-employees', req.url),
+//       );
+//     }
+//     return NextResponse.next();
+//   } catch (error) {
+//     return NextResponse.next(); // Proceed to next response in case of error
+//   }
+// }
 
-    if (!isExcludedPath && !token) {
-      return NextResponse.redirect(new URL('/authentication/login', req.url));
-    }
-    if (isExcludedPath && token) {
-      return NextResponse.redirect(
-        new URL('/employees/manage-employees', req.url),
-      );
-    }
-    return NextResponse.next();
-  } catch (error) {
-    return NextResponse.next(); // Proceed to next response in case of error
-  }
-}
-
-/*export function middleware() {
+export function middleware() {
   return NextResponse.next();
-}*/
+}
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
