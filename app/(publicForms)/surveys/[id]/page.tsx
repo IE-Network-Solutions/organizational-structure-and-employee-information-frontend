fb2094885dc9850ae9bf5d7c6afb25b795d4f17b
Abeselom Gebrekidan <abeselomgebrekidan12@gmail.com>
2024-09-13 14:37:37 +0300

    PPII-205 fix build

--- app/(publicForms)/surveys/[id]/page.tsx
+++ app/(publicForms)/surveys/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { Button, Col, Form, Row, Spin } from 'antd';
 import { useFetchPublicForms } from '@/store/server/features/feedback/dynamicForm/queries';
