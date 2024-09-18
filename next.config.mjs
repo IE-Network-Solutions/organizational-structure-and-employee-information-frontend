98fdbf3dd9bcdc725573b2f70a76bf39b63b077f
samiianteneh <samiianteneh5@gmail.com>
2024-09-18 10:36:22 +0300

    add notification URL

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const nextConfig = {
   env: {
     ORG_AND_EMP_URL: process.env.ORG_AND_EMP_URL,
     TENANT_MGMT_URL: process.env.TENANT_MGMT_URL,
+    NOTIFICATION_URL: process.env.NOTIFICATION_URL,
   },
 };
 
