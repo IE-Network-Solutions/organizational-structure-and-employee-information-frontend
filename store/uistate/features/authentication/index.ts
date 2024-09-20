af7329c02b87494577746db487d9abde3bc4eb91
ahmedahmedoumer <ahmedinoumer13@gmail.com>
2024-09-20 14:19:17 +0300

    PPII-271-changing the persister database from localStorge to indexDB

--- store/uistate/features/authentication/index.ts
+++ store/uistate/features/authentication/index.ts
@@ -1,6 +1,7 @@
 import { setCookie } from '@/helpers/storageHelper';
 import create from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
+import { set, get, del } from 'idb-keyval'; // Import idb-keyval methods
 
 interface StoreState {
   token: string;
@@ -23,28 +24,45 @@ export const useAuthenticationStore = create<StoreState>()(
       (set) => ({
         token: '',
         setToken: (token: string) => {
-          setCookie('token', token, 30), set({ token });
+          setCookie('token', token, 30); // Optionally set a cookie
+          set({ token });
         },
         tenantId: '',
-        setTenantId: (tenantId: string) => set({ tenantId }),
+        setTenantId: (tenantId: string) => {
+          set({ tenantId });
+        },
         userId: '',
-        setUserId: (userId: string) => set({ userId }),
+        setUserId: (userId: string) => {
+          set({ userId });
+        },
         localId: '',
-        setLocalId: (localId: string) => set({ localId }),
+        setLocalId: (localId: string) => {
+          set({ localId });
+        },
         loading: false, // Non-persistent state
-        setLoading: (loading: boolean) => set({ loading }), // Non-persistent method
+        setLoading: (loading: boolean) => set({ loading }),
         error: null, // Non-persistent state
-        setError: (error: string | null) => set({ error }), // Non-persistent method
+        setError: (error: string | null) => set({ error }),
       }),
       {
-        name: 'authentication-storage', // Unique name for the storage
-        getStorage: () => localStorage, // Use localStorage for persistence
+        name: 'authentications-storage', // Unique name for the storage
+        getStorage: () => ({
+          getItem: async (key: string) => {
+            const storedValue = await get(key); // Get item from IndexedDB
+            return storedValue ?? null;
+          },
+          setItem: async (key: string, value: any) => {
+            await set(key, value); // Set item in IndexedDB
+          },
+          removeItem: async (key: string) => {
+            await del(key); // Remove item from IndexedDB
+          },
+        }),
         partialize: (state) => ({
           token: state.token,
           tenantId: state.tenantId,
           localId: state.localId,
           userId: state.userId,
-          // 'loading' and 'error' are not included here, so they won't be persisted
         }),
       },
     ),
