148629815c707ac302427a2cf6d0fa18259dfcee
vadim-kropotov <dooondron@gmail.com>
2024-09-23 17:15:35 +0300

    add linter

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,6 @@ const nextConfig = {
     ORG_AND_EMP_URL: process.env.ORG_AND_EMP_URL,
     TENANT_MGMT_URL: process.env.TENANT_MGMT_URL,
   },
-  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
