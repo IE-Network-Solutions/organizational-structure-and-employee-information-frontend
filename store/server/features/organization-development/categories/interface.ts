233e0566e873027e1897bf0eaedc06d9e54ca649
Ifaol <ifaolhunde@gmail.com>
2024-11-11 09:07:20 +0300

    PPII-297-interface add from develop

--- store/server/features/organization-development/categories/interface.ts
+++ store/server/features/organization-development/categories/interface.ts
@@ -1,8 +1,35 @@
-import { UUID } from 'crypto';
-
 export type DataItem = {
-  name: string;
+  actionPlan: string;
   description: string;
-  jobTitleId: UUID;
-  userId: UUID;
+  responsiblePerson: string;
+};
+
+export type QuestionsType = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+  createdBy: string | null;
+  updatedBy: string | null;
+  formId: string;
+  question: string;
+  fieldType: string; // Add other field types as needed
+  field: any; // Update this with the correct type if known
+  order: number;
+  tenantId: string;
+  required: boolean;
+  responseDetail?: any[];
+};
+
+export type Meta = {
+  totalItems: number;
+  itemCount: number;
+  itemsPerPage: number;
+  totalPages: number;
+  currentPage: number;
+};
+
+export type QuestionData = {
+  items: QuestionsType[];
+  meta: Meta;
 };
