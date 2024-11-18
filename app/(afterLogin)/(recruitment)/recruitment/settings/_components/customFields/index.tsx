e889430e51e7138e022d21d010be57fdc9c0c676
lila-ana <gelilat1216@gmail.com>
2024-11-18 16:33:00 +0300

    Define Status

--- app/(afterLogin)/(recruitment)/recruitment/settings/_components/customFields/index.tsx
+++ app/(afterLogin)/(recruitment)/recruitment/settings/_components/customFields/index.tsx
@@ -27,31 +27,21 @@ const CustomAddJobFields: React.FC = () => {
           subtitle="Organizational Development Settings"
         />
       </div>
-      <Row gutter={[16, 24]}>
-        <Col lg={8} md={10} xs={24}>
-          <Card>
-            <div className="bg-[#F5F5F5] p-4 rounded-md flex items-center justify-center gap-2">
-              <CalendarDays size={18} color="#2f78ee" />
-              <p className="text-sm font-semibold">Custom fields</p>
-            </div>
-          </Card>
-        </Col>
-        <Col lg={16} md={14} xs={24}>
-          <Card>
-            <div className="flex items-center justify-between">
-              <Title level={5}>Custom Fields</Title>
-              <CustomButton
-                title="New Field"
-                id="createUserButton"
-                icon={<FaPlus size={13} className="mr-2" />}
-                onClick={showDrawer}
-                className="bg-blue-600 hover:bg-blue-700 h-12 py-5 text-medium font-semibold"
-              />
-            </div>
-            <CustomFieldsCard />
-          </Card>
-        </Col>
-      </Row>
+
+      <Card>
+        <div className="flex items-center justify-between">
+          <Title level={5}>Custom Fields</Title>
+          <CustomButton
+            title="New Field"
+            id="createUserButton"
+            icon={<FaPlus size={13} className="mr-2" />}
+            onClick={showDrawer}
+            className="bg-blue-600 hover:bg-blue-700 h-12 py-5 text-medium font-semibold"
+          />
+        </div>
+        <CustomFieldsCard />
+      </Card>
+
       <CustomFieldsDrawer onClose={onClose} />
     </div>
   );
