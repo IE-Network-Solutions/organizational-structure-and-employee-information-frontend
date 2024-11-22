a48fdb7ba595f8464f5752abbcd4ab87e02b6022
samiianteneh <samiianteneh5@gmail.com>
2024-11-22 08:19:27 +0300

    PPII-470 create Break type

--- types/timesheet/breakType.ts
+++ types/timesheet/breakType.ts
@@ -1,10 +1,13 @@
 import { DateInfo } from '@/types/commons/dateInfo';
 
-export interface BreakType extends DateInfo {
-  id: string;
-  tenantId: string;
+export interface BreakTypeList extends DateInfo {
+  id?: string;
+  tenantId?: string;
   title: string;
   description: string | null;
   startAt: string;
   endAt: string;
+}
+export interface BreakType {
+  item: BreakTypeList;
 }
