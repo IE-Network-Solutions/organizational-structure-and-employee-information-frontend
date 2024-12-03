2d7be3c3b0f7aacc8374c44c65cae231767024bb
Abeselom Gebrekidan <abeselomgebrekidan12@gmail.com>
2024-12-03 16:18:40 +0300

    remove unused imports

--- components/navBar/notificationBar.tsx
+++ components/navBar/notificationBar.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import { Avatar, Menu, Dropdown, Layout, Badge, Spin, Tooltip } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Menu, Dropdown,Badge, Spin, Tooltip } from 'antd';
 import { useNotificationDetailStore } from '@/store/uistate/features/notification';
 import { useGetNotifications } from '@/store/server/features/notification/queries';
 import { NotificationType } from '@/store/server/features/notification/interface';
