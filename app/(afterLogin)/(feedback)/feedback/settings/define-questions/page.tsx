74c00476479e907f74bd68ea994cb6262362218a
Ahmedin <ahmedinoumer13@gmail.com>
2024-12-06 17:06:33 +0300

    comments_on_conversation- finalize fetch conversation types and its question set

--- app/(afterLogin)/(feedback)/feedback/settings/define-questions/page.tsx
+++ app/(afterLogin)/(feedback)/feedback/settings/define-questions/page.tsx
@@ -6,61 +6,56 @@ import { ConversationStore } from "@/store/uistate/features/conversation";
 import { FaPlus } from "react-icons/fa";
 import { QuestionSet } from "../../conversation/[id]/_components/question-set";
 import QuestionSetForm from "../_components/questionSetForm";
+import { useConversationTypes } from "@/store/server/features/conversation/queries";
+import { ConversationTypeItems } from "@/store/server/features/conversation/conversationType/interface";
+import { Children, useEffect, useState } from "react";
+import ConversationTypeDetail from "./_components/ConversationTypeDetail/conversationTypeDetail";
 
 const Page = () => {
-    const { open, setOpen, setSearchField } = ConversationStore();
+    const { open, setOpen, setSearchField ,activeTab,setActiveTab} = ConversationStore();
+    const { data:getAllConversationType } = useConversationTypes();
+
 
   const onChange = (key: string) => {
-    console.log("Active Tab Key:", key);
+    setActiveTab(key);
   };
+  useEffect(()=>{
+    setActiveTab(getAllConversationType?.items[0]?.id)
+  },[getAllConversationType]);
+
   const onFinish = (key: string) => {
     console.log("Active Tab Key:", key);
   };
+  const activeTabName = getAllConversationType?.items?.find((item:ConversationTypeItems) => item.id === activeTab)?.name || '';
+
   const modalHeader = (
     <div className="flex justify-center text-xl font-extrabold text-gray-800 p-4">
-      Add New Bi-Weekly
-    </div>
+      Add New {activeTabName}
+      </div>
   );
-  const items: TabsProps['items'] = [
-    {
-        key: '1',
-        label: 'Bi-weekly',
-        children: '',
-      },
-      {
-        key: '2',
-        label: 'One-on-one',
-        children: '',
-      },
-      {
-        key: '3',
-        label: 'Healthy check-up',
-        children: '',
-      },
-      {
-        key: '4',
-        label: 'Survey',
-        children: '',
-      },
-      
-  ];
+  const items: TabsProps['items'] = getAllConversationType?.items?.map((item:ConversationTypeItems)=>
+      ({
+        key: item?.id,
+        label: item?.name,
+        children:<ConversationTypeDetail id={item?.id}/>
+      }));
+     
 
   return ( 
     <div>
         
       <div className="flex justify-between">
         <span className="font-bold text-lg">Questions</span>
-        <Button icon={<FaPlus/>} onClick={()=>setOpen(true)} type="primary"  className="h-10 text-xs">Add new Bi-Weekly</Button>
+        {activeTab!==''&&<Button icon={<FaPlus/>} onClick={()=>setOpen(true)} type="primary"  className="h-10 text-xs">Add new {activeTabName}</Button>}
       </div>
-
-      <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+      <Tabs defaultActiveKey={getAllConversationType?.items[0]?.id} items={items} onChange={onChange} />
       <CustomDrawerLayout
         open={open}
         onClose={() => setOpen(false)}
         modalHeader={modalHeader}
         width="40%"
       >
-        <QuestionSetForm/>
+        <QuestionSetForm  />
 
       </CustomDrawerLayout>
 
