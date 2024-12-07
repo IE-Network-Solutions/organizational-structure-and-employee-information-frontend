985ec36b813c6d8ca8903541a8d8cddd28b3d65f
Ahmedin <ahmedinoumer13@gmail.com>
2024-12-07 14:34:09 +0300

    PPII-recognitio-finalize server state for recognition critera

--- store/server/features/CFR/recognitionCriteria/mutation.ts
+++ store/server/features/CFR/recognitionCriteria/mutation.ts
@@ -1,84 +1,84 @@
-import { useAuthenticationStore } from "@/store/uistate/features/authentication";
-import { ORG_DEV_URL } from "@/utils/constants";
-import { crudRequest } from "@/utils/crudRequest";
-import { handleSuccessMessage } from "@/utils/showSuccessMessage";
-import { useMutation, useQueryClient } from "react-query";
+import { useAuthenticationStore } from '@/store/uistate/features/authentication';
+import { ORG_DEV_URL } from '@/utils/constants';
+import { crudRequest } from '@/utils/crudRequest';
+import { handleSuccessMessage } from '@/utils/showSuccessMessage';
+import { useMutation, useQueryClient } from 'react-query';
 
 const addRecognitionCriteria = async (data: any) => {
-    const token = useAuthenticationStore.getState().token;
-    const tenantId = useAuthenticationStore.getState().tenantId;
-    const createdBy = useAuthenticationStore.getState().userId;
-    const headers = {
-      tenantId: tenantId,
-      Authorization: `Bearer ${token}`,
-      createdByUserId: createdBy || '',
-    };
-    return await crudRequest({
-      url: `${ORG_DEV_URL}/recognition-criterias`,
-      method: 'POST',
-      data,
-      headers,
-    });
+  const token = useAuthenticationStore.getState().token;
+  const tenantId = useAuthenticationStore.getState().tenantId;
+  const createdBy = useAuthenticationStore.getState().userId;
+  const headers = {
+    tenantId: tenantId,
+    Authorization: `Bearer ${token}`,
+    createdByUserId: createdBy || '',
   };
-  const updateRecognitionCriteria = async (data: any) => {
-    const token = useAuthenticationStore.getState().token;
-    const tenantId = useAuthenticationStore.getState().tenantId;
-    const headers = {
-      tenantId: tenantId,
-      Authorization: `Bearer ${token}`,
-    };
-    return await crudRequest({
-      url: `${ORG_DEV_URL}/recognition-criterias/${data?.id}`,
-      method: 'patch',
-      data,
-      headers,
-    });
+  return await crudRequest({
+    url: `${ORG_DEV_URL}/recognition-criterias`,
+    method: 'POST',
+    data,
+    headers,
+  });
+};
+const updateRecognitionCriteria = async (data: any) => {
+  const token = useAuthenticationStore.getState().token;
+  const tenantId = useAuthenticationStore.getState().tenantId;
+  const headers = {
+    tenantId: tenantId,
+    Authorization: `Bearer ${token}`,
   };
-  const deleteRecognitionCriteia = async (id: any) => {
-    const token = useAuthenticationStore.getState().token;
-    const tenantId = useAuthenticationStore.getState().tenantId;
-    const createdBy = useAuthenticationStore.getState().userId;
-    const headers = {
-      tenantId: tenantId,
-      Authorization: `Bearer ${token}`,
-      createdByUserId: createdBy || '',
-    };
-    return await crudRequest({
-      url: `${ORG_DEV_URL}/recognition-criterias/${id}`,
-      method: 'delete',
-      headers,
-    });
+  return await crudRequest({
+    url: `${ORG_DEV_URL}/recognition-criterias/${data?.id}`,
+    method: 'patch',
+    data,
+    headers,
+  });
+};
+const deleteRecognitionCriteia = async (id: any) => {
+  const token = useAuthenticationStore.getState().token;
+  const tenantId = useAuthenticationStore.getState().tenantId;
+  const createdBy = useAuthenticationStore.getState().userId;
+  const headers = {
+    tenantId: tenantId,
+    Authorization: `Bearer ${token}`,
+    createdByUserId: createdBy || '',
   };
-  export const useUpdateRecognitionCriteria = () => {
-    const queryClient = useQueryClient();
-    return useMutation(updateRecognitionCriteria, {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      onSuccess: (_, variables: any) => {
-        queryClient.invalidateQueries('recognitionTypes');
-        const method = variables?.method?.toUpperCase();
-        handleSuccessMessage(method);
-      },
-    });
-  };
-  export const useDeleteRecognitionCriteria = () => {
-    const queryClient = useQueryClient();
-    return useMutation(deleteRecognitionCriteia, {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      onSuccess: (_, variables: any) => {
-        queryClient.invalidateQueries('recognitionTypes');
-        const method = variables?.method?.toUpperCase();
-        handleSuccessMessage(method);
-      },
-    });
-  };
+  return await crudRequest({
+    url: `${ORG_DEV_URL}/recognition-criterias/${id}`,
+    method: 'delete',
+    headers,
+  });
+};
+export const useUpdateRecognitionCriteria = () => {
+  const queryClient = useQueryClient();
+  return useMutation(updateRecognitionCriteria, {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    onSuccess: (_, variables: any) => {
+      queryClient.invalidateQueries('recognitionTypes');
+      const method = variables?.method?.toUpperCase();
+      handleSuccessMessage(method);
+    },
+  });
+};
+export const useDeleteRecognitionCriteria = () => {
+  const queryClient = useQueryClient();
+  return useMutation(deleteRecognitionCriteia, {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    onSuccess: (_, variables: any) => {
+      queryClient.invalidateQueries('recognitionTypes');
+      const method = variables?.method?.toUpperCase();
+      handleSuccessMessage(method);
+    },
+  });
+};
 export const useAddRecognitionCriteria = () => {
-    const queryClient = useQueryClient();
-    return useMutation(addRecognitionCriteria, {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      onSuccess: (_, variables: any) => {
-        queryClient.invalidateQueries('recognitionTypes');
-        const method = variables?.method?.toUpperCase();
-        handleSuccessMessage(method);
-      },
-    });
-  };
+  const queryClient = useQueryClient();
+  return useMutation(addRecognitionCriteria, {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    onSuccess: (_, variables: any) => {
+      queryClient.invalidateQueries('recognitionTypes');
+      const method = variables?.method?.toUpperCase();
+      handleSuccessMessage(method);
+    },
+  });
+};
--- store/server/features/CFR/recognitionCriteria/queries.ts
+++ store/server/features/CFR/recognitionCriteria/queries.ts
@@ -1,23 +1,22 @@
-import { useAuthenticationStore } from "@/store/uistate/features/authentication";
-import { ORG_DEV_URL } from "@/utils/constants";
-import { crudRequest } from "@/utils/crudRequest";
-import { useQuery } from "react-query"
+import { useAuthenticationStore } from '@/store/uistate/features/authentication';
+import { ORG_DEV_URL } from '@/utils/constants';
+import { crudRequest } from '@/utils/crudRequest';
+import { useQuery } from 'react-query';
 
 const getAllRecognitionTypes = async () => {
-    const token = useAuthenticationStore.getState().token;
-    const tenantId = useAuthenticationStore.getState().tenantId;
-  
-    return crudRequest({
-      url: `${ORG_DEV_URL}/recognition-type/parent`,
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        tenantId: tenantId,
-      },
-    });
-  };
+  const token = useAuthenticationStore.getState().token;
+  const tenantId = useAuthenticationStore.getState().tenantId;
+
+  return crudRequest({
+    url: `${ORG_DEV_URL}/recognition-type/parent`,
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      tenantId: tenantId,
+    },
+  });
+};
 
 export const useGetAllRecognitionType = () => {
-    return useQuery<any>('recognitionTypes', getAllRecognitionTypes);
-  };
-  
+  return useQuery<any>('recognitionTypes', getAllRecognitionTypes);
+};
