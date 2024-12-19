72ff7051b709ed3c2c4d904eb9e9cccbdcdb2662
surafel <surafelmohammed66@gmail.com>
2024-12-19 15:09:20 +0300

    PPII-533-i-have-a-job-list-from-a-json-file-but-the-system-cant-pass-the-job-id-to-the-frontend-to-populate-the-dropdown-list lint

--- app/(afterLogin)/(recruitment)/recruitment/jobs/[id]/_components/createCandidate/index.tsx
+++ app/(afterLogin)/(recruitment)/recruitment/jobs/[id]/_components/createCandidate/index.tsx
@@ -18,10 +18,7 @@ import cvUpload from '@/public/image/cvUpload.png';
 import { CandidateType } from '@/types/enumTypes';
 import { useCreateCandidate } from '@/store/server/features/recruitment/candidate/mutation';
 import { useAuthenticationStore } from '@/store/uistate/features/authentication';
-import {
-  useGetJobs,
-  useGetJobsByID,
-} from '@/store/server/features/recruitment/job/queries';
+import { useGetJobs } from '@/store/server/features/recruitment/job/queries';
 import { useGetStages } from '@/store/server/features/recruitment/candidate/queries';
 
 const { Dragger } = Upload;
