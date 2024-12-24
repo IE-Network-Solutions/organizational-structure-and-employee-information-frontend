cac1eb1b39b3cbc90b60fff534654c05c8effe38
Ahmedin <ahmedinoumer13@gmail.com>
2024-12-24 18:15:24 +0300

    PPII-recognition changeswrap by permission

--- app/(afterLogin)/(employeeInformation)/employees/manage-employees/page.tsx
+++ app/(afterLogin)/(employeeInformation)/employees/manage-employees/page.tsx
@@ -8,6 +8,8 @@ import UserTable from './_components/userTable';
 import { useEmployeeManagementStore } from '@/store/uistate/features/employees/employeeManagment';
 import EmployeeSearch from './_components/userSearch';
 import BlockWrapper from '@/components/common/blockWrapper/blockWrapper';
+import AccessGuard from '@/utils/permissionGuard';
+import { Permissions } from '@/types/commons/permissionEnum';
 
 const ManageEmployees: React.FC<any> = () => {
   const { setOpen } = useEmployeeManagementStore();
@@ -28,6 +30,7 @@ const ManageEmployees: React.FC<any> = () => {
             subtitle="Manage your Employees"
           />
           <div className="flex flex-wrap justify-start items-center my-4 gap-4 md:gap-8">
+          <AccessGuard permissions={[Permissions.RegisterNewEmployee]}>
             <CustomButton
               title="Create user"
               id="createUserButton"
@@ -35,6 +38,7 @@ const ManageEmployees: React.FC<any> = () => {
               onClick={showDrawer}
               className="bg-blue-600 hover:bg-blue-700"
             />
+            </AccessGuard>
             <UserSidebar onClose={onClose} />
           </div>
         </div>
