9270c91b0874f7ed20ce25819eb5496bdea0f826
samiianteneh <samiianteneh5@gmail.com>
2025-01-06 09:46:45 +0300

    PPII-578 integrate download button

--- app/(afterLogin)/(timesheetInformation)/timesheet/leave-management/leave-balance/_components/Download/index.tsx
+++ app/(afterLogin)/(timesheetInformation)/timesheet/leave-management/leave-balance/_components/Download/index.tsx
@@ -1,20 +1,11 @@
 import { useGetAllLeaveBalance } from '@/store/server/features/timesheet/leaveBalance/queries';
 import { Button } from 'antd';
-import React, { useEffect } from 'react';
-import { TbLayoutList } from 'react-icons/tb';
-
+import React from 'react';
 const DownloadLeaveBalance = () => {
   const buttonClass = 'text-xs font-bold h-[54px] w-full';
-  const {
-    data: allLeaveBalanceData,
-    isLoading: leaveBalanceIsLoading,
-    isFetched: allLeaveBalanceFetch,
-  } = useGetAllLeaveBalance();
+  const { data: allLeaveBalanceData } = useGetAllLeaveBalance();
   const onExport = () => {
-    window.open(allLeaveBalanceData, '_blank');
-    // window.open(
-    //   'https://files.ienetworks.co/view/test/9b320d7d-bece-4dd4-bb87-dd226f70daef/Total Time Card_Today, Dec 30.xlsx',
-    // );
+    window.open(allLeaveBalanceData?.file);
   };
 
   return (
@@ -23,7 +14,6 @@ const DownloadLeaveBalance = () => {
       id="excelFileTypeToExportId"
       className={buttonClass}
       type="primary"
-      //   icon={<TbLayoutList size={16} />}
       onClick={() => onExport()}
     >
       Download
