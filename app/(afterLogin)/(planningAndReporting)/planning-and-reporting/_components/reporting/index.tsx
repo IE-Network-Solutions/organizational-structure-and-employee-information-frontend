5ba79d7a94e96d244692274e48a2ee51bb11099b
ahmedahmedoumer <ahmedinoumer13@gmail.com>
2025-01-20 08:37:19 +0300

    finalize report reject,approve,delete and edit

--- app/(afterLogin)/(planningAndReporting)/planning-and-reporting/_components/reporting/index.tsx
+++ app/(afterLogin)/(planningAndReporting)/planning-and-reporting/_components/reporting/index.tsx
@@ -1,6 +1,6 @@
 import CustomButton from '@/components/common/buttons/customButton';
 import EmployeeSearch from '@/components/common/search/employeeSearch';
-import { Avatar, Card, Col, Row, Spin, Tooltip, Typography } from 'antd';
+import { Avatar, Button, Card, Col, Dropdown, Menu, Popconfirm, Row, Spin, Tooltip, Typography } from 'antd';
 import React from 'react';
 import { FaPlus } from 'react-icons/fa';
 import { MdOutlinePending } from 'react-icons/md';
@@ -21,17 +21,24 @@ import TasksDisplayer from './milestone';
 import Image from 'next/image';
 import CommentCard from '../comments/planCommentCard';
 import { UserOutlined } from '@ant-design/icons';
+import { IoIosOpen, IoMdMore } from 'react-icons/io';
+import { IoCheckmarkSharp } from 'react-icons/io5';
+import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
+import { useApprovalReporting, useDeleteReportById } from '@/store/server/features/okrPlanningAndReporting/mutations';
 
 const { Title } = Typography;
 
 function Reporting() {
-  const { setOpenReportModal, selectedUser, activePlanPeriod } =
+  const { setOpenReportModal, selectedUser, activePlanPeriod,setSelectedReportId,selectedReportId,setOpen,setEditing} =
     PlanningAndReportingStore();
   const { data: employeeData } = useGetAllUsers();
   const { userId } = useAuthenticationStore();
   const { data: departmentData } = useGetDepartmentsWithUsers();
   const { data: planningPeriods } = AllPlanningPeriods();
-  const planningPeriodId =
+  const { mutate: handleDeleteReport } = useDeleteReportById();
+  
+    const { mutate: ReportApproval, isLoading: isApprovalLoading } =useApprovalReporting();
+    const planningPeriodId =
     planningPeriods?.[activePlanPeriod - 1]?.planningPeriod?.id;
 
   const { data: allReporting, isLoading: getReportLoading } = useGetReporting({
@@ -50,6 +57,91 @@ function Reporting() {
 
     return employeeDataDetail || {}; // Return an empty object if employeeDataDetail is undefined
   };
+  const handleApproveHandler = (id: string, value: boolean) => {
+    const data = {
+      id: id,
+      value: value,
+    };
+    ReportApproval(data);
+  };
+
+
+  console.log(selectedReportId,"selectedReportId")
+  const actionsMenu = (
+    dataItem: any,
+    handleApproveHandler: any,
+    isApprovalLoading: any,
+  ) => (
+    <Menu>
+      {!dataItem?.isValidated ? (
+        <Menu.Item
+          icon={<IoCheckmarkSharp />}
+          onClick={() => handleApproveHandler(dataItem?.id, true)}
+          className="text-green-500"
+          key="approve"
+        >
+          <Tooltip
+            title={
+              isApprovalLoading
+                ? 'Processing approval...'
+                : "Approve Report! Once you approve, you can't edit"
+            }
+          >
+            Approve
+          </Tooltip>
+        </Menu.Item>
+       ) : ( 
+        <Menu.Item
+          className="text-red-400"
+          icon={<IoIosOpen size={16} />}
+          onClick={() => handleApproveHandler(dataItem?.id, false)}
+          key="reject"
+        >
+          <Tooltip title="Open approved Plan">Open</Tooltip>
+        </Menu.Item>
+       )} 
+    </Menu>
+  );
+  const actionsMenuEditandDelte = (
+    dataItem: any,
+    setEditing: any,
+    setSelectedReportId: any,
+    setOpen: any,
+  ) => (
+    <Menu>
+      {/* Edit Plan */}
+      <Menu.Item
+        icon={<AiOutlineEdit size={16} />}
+        onClick={() => {
+          setSelectedReportId(dataItem?.id);
+        }}
+        key="edit"
+      >
+        <Tooltip title="Edit Plan">
+          <span>Edit</span>
+        </Tooltip>
+      </Menu.Item>
+
+      {/* Delete Plan */}
+      <Menu.Item
+        className="text-red-400"
+        icon={<AiOutlineDelete size={16} />}
+        key="delete"
+      >
+        <Popconfirm
+          title="Are you sure you want to delete this plan?"
+          onConfirm={() => handleDeleteReport(dataItem?.id || '')}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Tooltip title="Delete Plan">
+            <span>Delete</span>
+          </Tooltip>
+        </Popconfirm>
+      </Menu.Item>
+    </Menu>
+  );
+
 
   return (
     <Spin spinning={getReportLoading} tip="Loading...">
@@ -66,7 +158,7 @@ function Reporting() {
                   allUnReportedPlanningTask.length > 0
                 )
               )
-                ? 'Plan tasks first or get manager approval'
+                ? 'Report tasks first or get manager approval'
                 : ''
             }
           >
@@ -147,11 +239,51 @@ function Reporting() {
                           span={10}
                           className="flex justify-end items-center"
                         >
+                          <>
                           <span className="mr-4 text-gray-500">
                             {dayjs(dataItem?.createdAt).format(
                               'MMMM D YYYY, h:mm:ss A',
                             )}
                           </span>
+                            {userId ===
+                             getEmployeeData(dataItem?.userId ?? dataItem?.createdBy)?.reportingTo?.id && ( 
+                              <Dropdown
+                                overlay={actionsMenu(
+                                  dataItem,
+                                  handleApproveHandler,
+                                  isApprovalLoading,
+                                )}
+                                trigger={['click']}
+                              >
+                                <Button
+                                  type="text"
+                                  icon={
+                                    <IoMdMore className="text-2xl font-bold" />
+                                  }
+                                  className="cursor-pointer text-green border-none  hover:text-success"
+                                />
+                              </Dropdown>
+                            )} 
+                             {userId === (dataItem?.userId ?? dataItem?.createdBy) &&
+                               dataItem?.isValidated == false && (
+                                <Dropdown
+                                  overlay={actionsMenuEditandDelte(
+                                    dataItem,
+                                    setEditing,
+                                    setSelectedReportId,
+                                    setOpen,
+                                  )}
+                                  trigger={['click']}
+                                >
+                                  <Button
+                                    type="text"
+                                    icon={<IoMdMore className="text-2xl" />}
+                                    className="cursor-pointer  text-black border-none  hover:text-primary"
+                                  />
+                                </Dropdown>
+                              )} 
+                          </>
+
                           <Col className="mr-2"></Col>
                           <Col></Col>
                         </Col>
--- store/uistate/features/planningAndReporting/useStore.ts
+++ store/uistate/features/planningAndReporting/useStore.ts
@@ -17,8 +17,10 @@ export interface PlanningAndReporting {
   setOpen: (open: boolean) => void;
   openReportModal: boolean;
   setOpenReportModal: (open: boolean) => void;
+
   isEditing: boolean;
   setEditing: (open: boolean) => void;
+  
   activeTab: number;
   setActiveTab: (activeTab: number) => void;
 
@@ -42,6 +44,10 @@ export interface PlanningAndReporting {
 
   selectedPlanId: string;
   setSelectedPlanId: (selectedPlanId: string) => void;
+
+
+  selectedReportId: string;
+  setSelectedReportId: (selectedReportId: string) => void;
 }
 const userId = useAuthenticationStore.getState().userId;
 export const PlanningAndReportingStore = create<PlanningAndReporting>()(
@@ -75,6 +81,9 @@ export const PlanningAndReportingStore = create<PlanningAndReporting>()(
 
     selectedPlanId: '',
     setSelectedPlanId: (selectedPlanId: string) => set({ selectedPlanId }),
+
+    selectedReportId: '',
+    setSelectedReportId: (selectedReportId: string) => set({ selectedReportId }),
 
     activePlanPeriod: 1,
     setActivePlanPeriod: (activePlanPeriod: number) =>
