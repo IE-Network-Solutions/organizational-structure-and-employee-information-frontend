8c52b3cf85afad8c23c508fbe3cb01a520b2f7b0
surafel <surafelmohammed66@gmail.com>
2025-01-23 10:58:39 +0300

    PPII-542-implement-payroll-page-ui generate and delete payroll using pay period

--- app/(afterLogin)/(payroll)/settings/layout.tsx
+++ app/(afterLogin)/(payroll)/settings/layout.tsx
@@ -70,23 +70,6 @@ const PayrollSettingsLayout: FC<OkrSettingsLayoutProps> = ({ children }) => {
     },
     {
       item: {
-        key: 'cost-sharing',
-        icon: (
-          <RiAwardFill
-            className={
-              currentItem === 'cost-sharing'
-                ? 'text-[#4DAEF0]'
-                : 'text-gray-500'
-            }
-          />
-        ),
-        label: <p className="font-bold text-sm text-gray-900">Cost Sharing</p>,
-        className: currentItem === 'cost-sharing' ? 'px-4' : 'px-1',
-      },
-      link: '/settings/cost-sharing',
-    },
-    {
-      item: {
         key: 'pay-period',
         icon: (
           <FaBomb
@@ -100,21 +83,21 @@ const PayrollSettingsLayout: FC<OkrSettingsLayoutProps> = ({ children }) => {
       },
       link: '/settings/pay-period',
     },
-    {
-      item: {
-        key: 'banks',
-        icon: (
-          <TbTargetArrow
-            className={
-              currentItem === 'banks' ? 'text-[#4DAEF0]' : 'text-gray-500'
-            }
-          />
-        ),
-        label: <p className="font-bold text-sm text-gray-900">Banks</p>,
-        className: currentItem === 'banks' ? 'px-4' : 'px-1',
-      },
-      link: '/settings/banks',
-    },
+    // {
+    //   item: {
+    //     key: 'banks',
+    //     icon: (
+    //       <TbTargetArrow
+    //         className={
+    //           currentItem === 'banks' ? 'text-[#4DAEF0]' : 'text-gray-500'
+    //         }
+    //       />
+    //     ),
+    //     label: <p className="font-bold text-sm text-gray-900">Banks</p>,
+    //     className: currentItem === 'banks' ? 'px-4' : 'px-1',
+    //   },
+    //   link: '/settings/banks',
+    // },
   ]);
 
   useEffect(() => {
