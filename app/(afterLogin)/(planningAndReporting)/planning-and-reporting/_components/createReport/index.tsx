345d9bd7ba9b76b684d6454388b5e72bd60bfa29
Abeselom Gebrekidan <abeselomgebrekidan12@gmail.com>
2025-01-31 18:11:28 +0300

    Improve code readability by formatting the assignment of formattedData in CreateReport component

--- app/(afterLogin)/(planningAndReporting)/planning-and-reporting/_components/createReport/index.tsx
+++ app/(afterLogin)/(planningAndReporting)/planning-and-reporting/_components/createReport/index.tsx
@@ -86,7 +86,8 @@ function CreateReport() {
   };
 
   const formattedData =
-    allPlannedTaskForReport && groupUnReportedTasksByKeyResultAndMilestone(allPlannedTaskForReport);
+    allPlannedTaskForReport &&
+    groupUnReportedTasksByKeyResultAndMilestone(allPlannedTaskForReport);
 
   const totalWeight = formattedData?.reduce((sum: number, objective: any) => {
     return (
