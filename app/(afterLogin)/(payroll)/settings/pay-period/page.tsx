d92a3710133c3925b40b2d6590b91be689ff4026
surafel <surafelmohammed66@gmail.com>
2025-02-11 08:22:32 +0300

    PPII-723-remove-delete-action-button-or-add-edit-action-button-to-insert-deleted-month

--- app/(afterLogin)/(payroll)/settings/pay-period/page.tsx
+++ app/(afterLogin)/(payroll)/settings/pay-period/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { Table, Button, Space, Typography, Switch, Spin } from 'antd';
-import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+import { EditOutlined, PlusOutlined } from '@ant-design/icons';
 import PayPeriodSideBar from './_components/payPeriodSideBar';
 import usePayPeriodStore from '@/store/uistate/features/payroll/settings/payPeriod';
 import { useFetchActiveFiscalYearPayPeriods } from '@/store/server/features/payroll/setting/tax-rule/queries';
@@ -13,6 +13,7 @@ import {
 import dayjs from 'dayjs';
 import AccessGuard from '@/utils/permissionGuard';
 import { Permissions } from '@/types/commons/permissionEnum';
+import PayPeriodEditSidebar from './_components/payPeroidEditSideBar';
 const { Title } = Typography;
 interface DataSource {
   key: string;
@@ -32,7 +33,7 @@ const PayPeriod = () => {
     setPageSize,
   } = usePayPeriodStore();
   const { data: activeFiscalYear } = useGetActiveFiscalYears();
-  const { mutate: deletePayPeriod } = useDeletePayPeriod();
+  // const { mutate: deletePayPeriod } = useDeletePayPeriod();
   const { mutate: changePayPeriodStatus } = useChangePayPeriodStatus();
   const { data: payPeriods, isLoading } = useFetchActiveFiscalYearPayPeriods(
     activeFiscalYear?.id,
@@ -41,8 +42,8 @@ const PayPeriod = () => {
   const handleAddPayPeriod = () => {
     setIsPayPeriodSidebarVisible(true);
   };
-  const handleDeletePayPeriod = (payPeriodId: string) => {
-    deletePayPeriod(payPeriodId);
+  const handleEditPayPeriod = (payPeriodId: string) => {
+    // deletePayPeriod(payPeriodId);
   };
 
   const handleTableChange = (pagination: any) => {
@@ -103,9 +104,8 @@ const PayPeriod = () => {
             />
             <Button
               type="primary"
-              danger
-              icon={<DeleteOutlined />}
-              onClick={() => handleDeletePayPeriod(record.id)}
+              icon={<EditOutlined />}
+              onClick={() => handleEditPayPeriod(record)}
             />
           </Space>
         </AccessGuard>
@@ -142,6 +142,7 @@ const PayPeriod = () => {
         />
       </Spin>
       <PayPeriodSideBar />
+      <PayPeriodEditSidebar />
     </div>
   );
 };
