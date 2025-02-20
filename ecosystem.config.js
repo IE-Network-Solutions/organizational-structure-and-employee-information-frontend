f410ab8a698122d4fa2ec86c1f85e729ce197ad1
Yonas Tewabe <yonastewabe21@gmail.com>
2025-02-20 15:01:08 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'osei-front-app',
-      script: 'npm',
-      args: 'run start',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3001',
       env: {
+        NODE_ENV: 'production',
         PORT: 3001,
       },
     },
