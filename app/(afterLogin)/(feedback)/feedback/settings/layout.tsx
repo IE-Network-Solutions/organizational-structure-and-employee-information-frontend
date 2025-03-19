1b2edcbdaf6331ffcb5710d965598dfe2f743a1d
Bini <binilulseged@gmail.com>
2025-03-19 14:53:43 +0300

    remove unnecessary whitespace in PositionCards component

--- app/(afterLogin)/(feedback)/feedback/settings/layout.tsx
+++ app/(afterLogin)/(feedback)/feedback/settings/layout.tsx
@@ -4,7 +4,6 @@ import PageHeader from '@/components/common/pageHeader/pageHeader';
 import BlockWrapper from '@/components/common/blockWrapper/blockWrapper';
 import { SidebarMenuItem } from '@/types/sidebarMenu';
 import SidebarMenu from '@/components/sidebarMenu';
-import { TfiMedallAlt } from 'react-icons/tfi';
 import { GoQuestion } from 'react-icons/go';
 import { RiFeedbackLine } from 'react-icons/ri';
 import { CiCalendarDate } from 'react-icons/ci';
--- app/(afterLogin)/(payroll)/settings/layout.tsx
+++ app/(afterLogin)/(payroll)/settings/layout.tsx
@@ -1,10 +1,8 @@
 'use client';
 import { FC, ReactNode, useEffect, useState } from 'react';
-import { TbLayoutList } from 'react-icons/tb';
 import { usePathname, useRouter } from 'next/navigation';
-import { FaBomb } from 'react-icons/fa';
 import PageHeader from '@/components/common/pageHeader/pageHeader';
-import { Card, ConfigProvider, Menu, MenuProps } from 'antd';
+import {  ConfigProvider, Menu, MenuProps } from 'antd';
 import { MdOutlinePayments } from 'react-icons/md';
 import { HiOutlineReceiptTax } from 'react-icons/hi';
 import { GiSuspensionBridge } from 'react-icons/gi';
