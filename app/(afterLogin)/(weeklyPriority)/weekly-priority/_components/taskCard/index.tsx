e3fa68ed0578a8997292a7c3c6dbdda2350abe71
Abeselom Gebrekidan <abeselomgebrekidan12@gmail.com>
2025-03-22 07:46:46 +0300

    Update index.tsx

--- app/(afterLogin)/(weeklyPriority)/weekly-priority/_components/taskCard/index.tsx
+++ app/(afterLogin)/(weeklyPriority)/weekly-priority/_components/taskCard/index.tsx
@@ -27,7 +27,7 @@ const TaskCard: React.FC = () => {
   const { data: activeFiscalYear } = useGetActiveFiscalYears();
 
   const session = activeFiscalYear?.sessions?.find(
-    (item: any) => !item?.active,
+    (item: any) => item?.active===true,
   );
   const month = session?.months?.find((item: any) => item?.active===true);
   const userDepartmentId = userInfo?.employeeJobInformation[0]?.departmentId;
