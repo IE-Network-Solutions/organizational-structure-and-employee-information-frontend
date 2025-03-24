58a6e45bedfbde351638a260993888b04e9dbf0d
bessonniy11 <50315006+bessonniy11@users.noreply.github.com>
2025-03-24 11:31:57 +0300

    Update ecosystem configuration for front-end apps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'osei-front-app',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3001',
+      name: 'pep_front_stage',
+      script: 'npm',
+      args: 'run start',
       env: {
-        NODE_ENV: 'production',
-        PORT: 3001,
+        PORT: 9412,
+      },
+    },
+    {
+      name: 'pep_front_dev',
+      script: 'npm',
+      args: 'run start',
+      env: {
+        PORT: 9411,
       },
     },
   ],
