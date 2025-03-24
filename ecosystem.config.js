07c182fafe6661d630852eec4057cd3b81d083b4
bessonniy11 <50315006+bessonniy11@users.noreply.github.com>
2025-03-24 17:01:28 +0300

    Enhance ecosystem and configuration for front-end apps
    - Added new app configuration for 'osei-front-app' in ecosystem.config.js.
    - Updated PORT settings in ecosystem.config.js and next.config.mjs to use environment variable NEXT_PUBLIC_PORT.
    - Modified start script in package.json to support dynamic port assignment using NEXT_PUBLIC_PORT.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   apps: [
     {
+      name: 'osei-front-app',
+      script: 'npm',
+      args: 'run start',
+      env: {
+        PORT: process.env.NEXT_PUBLIC_PORT || 3001,
+      }
+    },
+    {
       name: 'pep_front_stage',
       script: 'npm',
       args: 'run start',
       env: {
-        PORT: 9412,
+        PORT: process.env.NEXT_PUBLIC_PORT || 9412,
       },
     },
     {
@@ -13,7 +21,7 @@ module.exports = {
       script: 'npm',
       args: 'run start',
       env: {
-        PORT: 9411,
+        PORT: process.env.NEXT_PUBLIC_PORT || 9411,
       },
     },
   ],
