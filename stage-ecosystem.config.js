8980752ed92dc2217b410d5c16bfca63a1afb633
Yonas Tewabe <yonastewabe21@gmail.com>
2025-03-25 17:14:42 +0300

    Update stage-ecosystem.config.js

--- stage-ecosystem.config.js
+++ stage-ecosystem.config.js
@@ -1,1 +1,14 @@
 
+module.exports = {
+  apps: [
+    {
+      name: 'staging-osei-front-app',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3005',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3005,
+      },
+    },
+  ],
+};
