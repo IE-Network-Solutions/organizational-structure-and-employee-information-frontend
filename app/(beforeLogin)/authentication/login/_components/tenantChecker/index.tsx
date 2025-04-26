89b1f64340a600cdfbca44998d2667c39c551690
surpog17 <surafelkifle17@gmail.com>
2025-04-26 12:37:40 +0300

    refactor: Clean up code formatting and remove unnecessary console log in tenantChecker

--- app/(beforeLogin)/authentication/login/_components/tenantChecker/index.tsx
+++ app/(beforeLogin)/authentication/login/_components/tenantChecker/index.tsx
@@ -5,13 +5,12 @@ import { useAuthenticationStore } from '@/store/uistate/features/authentication'
 import { useEffect } from 'react';
 
 export const useTenantChecker = () => {
-  const {hostname, setHostName} = useAuthenticationStore();
+  const { hostname, setHostName } = useAuthenticationStore();
   useEffect(() => {
     setHostName(window.location.hostname);
   }, []);
   const domainName = hostname?.split('.')[0] || hostname;
   const { data: tenantInfo, refetch } = useGetTenantByDomain(domainName || '');
-  console.log('tenantInfo', domainName);
   useEffect(() => {
     if (domainName) refetch();
   }, [domainName]);
