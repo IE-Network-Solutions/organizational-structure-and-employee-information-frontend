c2ed452ad86d70c07d7e39049adaaae58d3a7f08
Yonas Tewabe <yonastewabe21@gmail.com>
2025-05-08 09:44:18 +0300

    Create preview-ecosystem.config.js

---
+++ preview-ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'osei-front-app-preview',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3005',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3005,
+      },
+    },
+  ],
+};
