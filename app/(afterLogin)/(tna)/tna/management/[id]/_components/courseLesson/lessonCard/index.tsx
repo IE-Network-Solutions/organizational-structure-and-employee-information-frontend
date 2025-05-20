1446d6067ba2b43b4196a0a681ad42114ea9a835
ahmedahmedoumer <ahmedahmedoumer@gmail.com>
2025-05-20 08:52:17 +0300

    finalize making the course lesson matrrial should be ordered based on the order given

--- app/(afterLogin)/(tna)/tna/management/[id]/_components/courseLesson/lessonCard/index.tsx
+++ app/(afterLogin)/(tna)/tna/management/[id]/_components/courseLesson/lessonCard/index.tsx
@@ -78,7 +78,9 @@ const LessonCard: FC<LessonCardProps> = ({ lesson }) => {
             <div className="pl-9 flex">
               <div className="flex-1">
                 {lesson.courseLessonMaterials.length ? (
-                  lesson.courseLessonMaterials.map((item) => (
+                  [...lesson.courseLessonMaterials]
+                    .sort((a, b) => a.order - b.order)
+                    .map((item, index) => (
                     <div
                       className="flex items-center justify-between mb-1 last:mb-0"
                       key={item.id}
@@ -86,12 +88,12 @@ const LessonCard: FC<LessonCardProps> = ({ lesson }) => {
                       <Link
                         id="tnaRedirectToTnaManagment"
                         href={`/tna/management/${course?.id}/${lesson.id}/${item.id}`}
-                        className="text-sm text-gray-600 hover:text-primary  w-full md:w-auto pr-2"
+                        className="text-sm text-gray-600 hover:text-primary w-full md:w-auto pr-2"
                       >
-                        {item.title}
+                        {`${index + 1}. ${item.title}`}
                       </Link>
 
-                      <div className="flex items-center gap-2 w-24  min-w-[100px]">
+                      <div className="flex items-center gap-2 w-24 min-w-[100px]">
                         <div className="w-1 h-1 rounded-full bg-gray-900"></div>
                         <div className="text-xs text-gray-400">
                           {item.timeToFinishMinutes} minutes
