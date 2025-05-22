422d2bcdf80ca7e776f3bb4f8b123b5f1f273818
tesfahunBirega <tesfubirega@gmail.com>
2025-05-22 11:40:11 +0300

    Modifying TNA Creates New TNA Record

--- app/(afterLogin)/(okrplanning)/okr/_components/dashboard/objectivecard/index.tsx
+++ app/(afterLogin)/(okrplanning)/okr/_components/dashboard/objectivecard/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Progress, Card, Avatar, Menu, Dropdown } from 'antd';
-import { PiUserCircleCheck } from 'react-icons/pi';
-
+import { PiCalendarMinusBold } from 'react-icons/pi';
 import KeyResultMetrics from '../keyresultmetrics';
 import EditObjective from '../editObjective';
 import { useOKRStore } from '@/store/uistate/features/okrplanning/okr';
@@ -155,7 +154,7 @@ const ObjectiveCard: React.FC<ObjectiveProps> = ({ objective, myOkr }) => {
                   className={`${isMobile ? 'gap-2 items-center justify-between w-full' : 'grid items-center gap-0'}`}
                 >
                   <div className="flex items-center">
-                    <PiUserCircleCheck className="text-blue mt-1" />
+                    <PiCalendarMinusBold className="text-blue mt-1" />
                     <div
                       className={`font-bold text-[#3636f0] ${isMobile ? 'text-lg ml-2' : 'text-2xl'}`}
                     >
