8ddb44c95ee34712a1939d38587192a9f9a2a375
surpog17 <surafelkifle17@gmail.com>
2025-06-05 15:45:04 +0300

    Refactor: Clean up and optimize employee profile and filters components

--- app/(afterLogin)/(payroll)/payroll/_components/filters/index.tsx
+++ app/(afterLogin)/(payroll)/payroll/_components/filters/index.tsx
@@ -9,7 +9,6 @@ import {
 import dayjs from 'dayjs';
 import { useTnaReviewStore } from '@/store/uistate/features/tna/review';
 import { useGetDepartments } from '@/store/server/features/employees/employeeManagment/department/queries';
-import { usePayrollStore } from '@/store/uistate/features/payroll/payroll';
 import useEmployeeStore from '@/store/uistate/features/payroll/employeeInfoStore';
 
 const { Option } = Select;
@@ -39,8 +38,6 @@ const Filters: React.FC<FiltersProps> = ({
   const { data: payPeriodData } = useGetPayPeriod();
   const { data: departmentData } = useGetDepartments();
   const { searchQuery, pageSize, currentPage } = useEmployeeStore();
-
-  const { pageSize, currentPage } = usePayrollStore();
   const { data: payroll } = useGetActivePayroll(
     searchQuery,
     pageSize,
