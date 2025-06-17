4d5522cb4988b885f1147e991eb95f3f24033b92
lila-ana <gelilat1216@gmail.com>
2025-06-17 16:33:47 +0300

    PPII-1279 corrected branch conflict

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -52,6 +52,8 @@ const Nav: React.FC<MyComponentProps> = ({ children }) => {
   const pathname = usePathname();
   const { userId } = useAuthenticationStore();
   const { isLoading } = useGetEmployee(userId);
+  const { userData } = useAuthenticationStore();
+
   const {
     setLocalId,
     setTenantId,
@@ -64,6 +66,8 @@ const Nav: React.FC<MyComponentProps> = ({ children }) => {
     setIs2FA,
     setTwoFactorAuthEmail,
     setUser2FA,
+    isCheckingPermissions,
+    setIsCheckingPermissions,
   } = useAuthenticationStore();
   const isAdminPage = pathname.startsWith('/admin');
 
@@ -940,16 +944,22 @@ const Nav: React.FC<MyComponentProps> = ({ children }) => {
             transition: 'padding-left 0.3s ease',
           }}
         >
-          <div
-            className={`overflow-auto ${!isAdminPage ? 'bg-white' : ''}`}
-            style={{
-              borderRadius: borderRadiusLG,
-              marginTop: '94px',
-              marginRight: `${isMobile ? 0 : !isAdminPage ? '0px' : ''}`,
-            }}
-          >
-            {children}
-          </div>
+          {isCheckingPermissions ? (
+            <div className="flex justify-center items-center h-screen">
+              <Skeleton active />
+            </div>
+          ) : (
+            <div
+              className={`overflow-auto ${!isAdminPage ? 'bg-white' : ''}`}
+              style={{
+                borderRadius: borderRadiusLG,
+                marginTop: '94px',
+                marginRight: `${isMobile ? 0 : !isAdminPage ? '0px' : ''}`,
+              }}
+            >
+              {children}
+            </div>
+          )}
         </Content>
       </Layout>
     </Layout>
