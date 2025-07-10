d69b62beb043030d5cba47d99ac46f36df802605
lila-ana <gelilat1216@gmail.com>
2025-07-10 11:08:31 +0300

    Enhance IncentiveSettingsDrawer and DefaultIncentiveSettingsTable components. Update handleClose function to conditionally clear form fields. Improve loading state display in the incentive settings table with Skeleton components for better user experience.

--- app/(afterLogin)/(incentive)/incentives/settings/[id]/_components/incentiveSettingdrawer/index.tsx
+++ app/(afterLogin)/(incentive)/incentives/settings/[id]/_components/incentiveSettingdrawer/index.tsx
@@ -60,11 +60,15 @@ const IncentiveSettingsDrawer: React.FC<IncentiveSettingsDrawerProps> = ({
   );
   //   ===========> Functions <============
 
-  const handleClose = () => {
+  const handleClose = (shouldClear = false) => {
     setOpenIncentiveDrawer(false);
-    setValue('');
-    setIncentiveId('');
-    form.resetFields();
+    if (shouldClear) {
+      setValue('');
+      setIncentiveId('');
+      setFormula([]);
+      setFormulaError('');
+      form.resetFields();
+    }
   };
 
   const handleRadioChange = ({ target: { value } }: RadioChangeEvent) => {
@@ -467,7 +471,7 @@ const IncentiveSettingsDrawer: React.FC<IncentiveSettingsDrawerProps> = ({
   return (
     <CustomDrawerLayout
       open={openIncentiveDrawer}
-      onClose={handleClose}
+      onClose={() => handleClose(false)}
       modalHeader={
         <CustomDrawerHeader className="flex justify-center ">
           {recognitionData?.name || '-'}
@@ -477,7 +481,7 @@ const IncentiveSettingsDrawer: React.FC<IncentiveSettingsDrawerProps> = ({
         <div className="flex justify-center  w-full p-4 gap-6">
           <Button
             type="default"
-            onClick={handleClose}
+            onClick={() => handleClose(true)}
             className=" p-4 px-10 h-10 "
           >
             Cancel
