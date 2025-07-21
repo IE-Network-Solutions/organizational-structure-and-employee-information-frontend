45c58673d7e85c51aaac2356629826e7d6482f79
surafelmohammed <surafelmohammed66@gmail.com>
2025-07-21 03:24:18 -0700

    comments

--- app/(afterLogin)/(okrplanning)/okr/_components/keyresultForm/numericForm/index.tsx
+++ app/(afterLogin)/(okrplanning)/okr/_components/keyresultForm/numericForm/index.tsx
@@ -57,7 +57,7 @@ const NumericForm: React.FC<OKRFormProps> = ({
       >
         {/* Desktop Layout */}
         <div
-          className={`${isMobile ? 'hidden' : 'flex'} flex-row gap-1 items-center mt-4 mx-4`}
+          className={`${isMobile ? 'hidden' : 'flex'} flex-row gap-2 items-center mt-4 mx-4`}
         >
           <Form.Item
             className="flex-1 mr-2 mb-0"
@@ -159,7 +159,7 @@ const NumericForm: React.FC<OKRFormProps> = ({
         </div>
         {/* Desktop Numeric input row */}
         <div
-          className={`${isMobile ? 'hidden' : 'flex'} flex-row gap-6 items-center mt-4 mx-4`}
+          className={`${isMobile ? 'hidden' : 'flex'} flex-row gap-4 items-center mt-4 mx-4`}
         >
           <Form.Item
             className="w-60 mb-0"
@@ -322,7 +322,7 @@ const NumericForm: React.FC<OKRFormProps> = ({
             </Form.Item>
           </div>
           {/* Row 3: Initial Value and Target Value */}
-          <div className="flex gap-6 pl-4">
+          <div className="flex gap-4 pl-4">
             <Form.Item
               className="flex-1 mb-0"
               name="initialValue"
