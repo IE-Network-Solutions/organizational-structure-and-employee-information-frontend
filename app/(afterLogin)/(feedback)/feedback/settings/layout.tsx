f0637dd463d63db27640a2e8cabe0de3c6899e88
bahabelom <bahabelomgebremedhn2@gmail.com>
2025-08-05 10:52:01 +0300

    PPII-1369 “Define Questions” Should Be Removed from CFR Settings Page

--- app/(afterLogin)/(feedback)/feedback/settings/layout.tsx
+++ app/(afterLogin)/(feedback)/feedback/settings/layout.tsx
@@ -34,23 +34,23 @@ const CFRSettingLayout: FC<TimesheetSettingsLayoutProps> = ({ children }) => {
       },
       link: '/feedback/settings/define-feedback',
     },
-    {
-      item: {
-        key: 'define-questions',
-        icon: (
-          <div
-            className={`lg:flex items-center gap-2 ${pathname.includes('/feedback/settings/define-questions') ? 'lg:ml-4' : ''}`}
-          >
-            <GoQuestion
-              className={`hidden lg:block ${pathname.includes('/feedback/settings/define-questions') ? 'text-[#1677FF]' : ''}`}
-            />
-            <p className="menu-item-label">Define Questions</p>
-          </div>
-        ),
-        className: 'px-1',
-      },
-      link: '/feedback/settings/define-questions',
-    },
+    // {
+    //   item: {
+    //     key: 'define-questions',
+    //     icon: (
+    //       <div
+    //         className={`lg:flex items-center gap-2 ${pathname.includes('/feedback/settings/define-questions') ? 'lg:ml-4' : ''}`}
+    //       >
+    //         <GoQuestion
+    //           className={`hidden lg:block ${pathname.includes('/feedback/settings/define-questions') ? 'text-[#1677FF]' : ''}`}
+    //         />
+    //         <p className="menu-item-label">Define Questions</p>
+    //       </div>
+    //     ),
+    //     className: 'px-1',
+    //   },
+    //   link: '/feedback/settings/define-questions',
+    // },
 
     // {
     //   item: {
