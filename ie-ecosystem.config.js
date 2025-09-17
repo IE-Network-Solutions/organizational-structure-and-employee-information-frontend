729f387dfec8c8d64ab014a4b5f06fc40ffca4a1
Yonas Tewabe <yonastewabe21@gmail.com>
2025-09-17 09:15:49 +0300

    Update ie-ecosystem.config.js

--- ie-ecosystem.config.js
+++ ie-ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'osei-front-app',
+      name: 'osei-front-app-ie',
       script: 'node_modules/next/dist/bin/next',
       args: 'start -p 9002',
       env: {
