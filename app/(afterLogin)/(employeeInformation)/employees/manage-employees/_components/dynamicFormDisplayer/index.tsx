215025f587b52dd2a195160edf2216d8c7ad94d8
abelendeshaw <contact.abelendeshaw@gmail.com>
2025-10-06 16:40:49 +0300

    added reqired functionality for custom fields

--- app/(afterLogin)/(employeeInformation)/employees/manage-employees/_components/dynamicFormDisplayer/index.tsx
+++ app/(afterLogin)/(employeeInformation)/employees/manage-employees/_components/dynamicFormDisplayer/index.tsx
@@ -2,135 +2,136 @@ import React from 'react';
 import {
   Form,
   Input,
-  Select,
   DatePicker,
-  Checkbox,
+  Select,
   Switch,
-  Row,
+  Checkbox,
   Col,
+  Row,
 } from 'antd';
-import { validateField } from '../formValidator';
 
 const { Option } = Select;
 
-interface FormField {
-  id: string;
-  fieldType: 'input' | 'select' | 'datePicker' | 'checkbox' | 'toggle';
-  isActive: boolean;
-  fieldName: string;
-  fieldValidation: string;
-  options?: string[]; // Options for 'select', 'checkbox'
-}
-
 interface DynamicFormFieldsProps {
-  fields: FormField[];
   formTitle: string;
+  fields: any[];
 }
 
 const DynamicFormFields: React.FC<DynamicFormFieldsProps> = ({
   formTitle,
   fields,
 }) => {
-  const renderField = (field: FormField) => {
-    if (!field.isActive) return null; // Skip inactive fields
+  const renderField = (field: any) => {
+    // Only render active fields
+    if (!field.isActive) return null;
 
-    const commonProps = {
-      className: 'font-semibold text-xs',
-      label: field.fieldName,
-      name: [formTitle, field.fieldName],
-      id: `${formTitle}${field.fieldName}`,
-      rules: [
-        {
-          /*  eslint-disable-next-line @typescript-eslint/naming-convention */
-          validator: (_rule: any, value: any) => {
-            /*  eslint-enable-next-line @typescript-eslint/naming-convention */
-            const validationError = validateField(
-              field.fieldType,
-              value,
-              field.fieldValidation,
-            );
-            if (validationError)
-              return Promise.reject(new Error(validationError));
-            // if (field.fieldType === 'input') {
-            //   const nameError = validateName(field.fieldName, value);
-            //   if (nameError) return Promise.reject(new Error(nameError));
-            // }
-            return Promise.resolve();
-          },
-        },
-      ],
-    };
+    const fieldName = [formTitle, field.fieldName];
 
-    switch (field.fieldType) {
-      case 'input':
-        return (
-          <Form.Item key={field.fieldName} {...commonProps}>
-            <Input />
-          </Form.Item>
-        );
-      case 'select':
-        return (
-          <Form.Item key={field.fieldName} {...commonProps}>
-            <Select>
-              {field.options?.map((option) => (
-                <Option key={option} value={option}>
+    // Build rules array based on isRequired
+    const rules: any[] = [];
+
+    if (field.isRequired) {
+      rules.push({
+        required: true,
+        message: `${field.fieldName.replace(/_/g, ' ')} is required`,
+      });
+    }
+
+    // Add validation rules based on fieldValidation
+    if (field.fieldValidation === 'email') {
+      rules.push({
+        type: 'email',
+        message: 'Please enter a valid email',
+      });
+    } else if (field.fieldValidation === 'url') {
+      rules.push({
+        type: 'url',
+        message: 'Please enter a valid URL',
+      });
+    } else if (field.fieldValidation === 'number') {
+      rules.push({
+        pattern: /^[0-9]+$/,
+        message: 'Please enter only numbers',
+      });
+    } else if (field.fieldValidation === 'date') {
+      rules.push({
+        type: 'date',
+        message: 'Please enter a valid date',
+      });
+    }
+
+    const renderInput = () => {
+      switch (field.fieldType) {
+        case 'input':
+          return (
+            <Input
+              placeholder={`Enter ${field.fieldName.replace(/_/g, ' ')}`}
+            />
+          );
+        case 'datePicker':
+          return (
+            <DatePicker
+              className="w-full"
+              placeholder={`Select ${field.fieldName.replace(/_/g, ' ')}`}
+            />
+          );
+        case 'select':
+          return (
+            <Select
+              placeholder={`Select ${field.fieldName.replace(/_/g, ' ')}`}
+              allowClear
+            >
+              {field.options?.map((option: string, index: number) => (
+                <Option key={index} value={option}>
                   {option}
                 </Option>
               ))}
             </Select>
-          </Form.Item>
-        );
-      case 'datePicker':
-        return (
-          <Form.Item key={field.fieldName} {...commonProps} className="w-full">
-            <DatePicker />
-          </Form.Item>
-        );
-      case 'checkbox':
-        return (
-          <Form.Item key={field.fieldName} {...commonProps}>
-            <Checkbox.Group>
-              {field.options?.map((option) => (
-                <Checkbox key={option} value={option}>
-                  {option}
-                </Checkbox>
-              ))}
-            </Checkbox.Group>
-          </Form.Item>
-        );
-      case 'toggle':
-        return (
-          <Form.Item
-            key={field.fieldName}
-            {...commonProps}
-            valuePropName="checked"
-          >
-            <Switch />
-          </Form.Item>
-        );
-      default:
-        return null;
-    }
+          );
+        case 'toggle':
+          return <Switch />;
+        case 'checkbox':
+          return <Checkbox>{field.fieldName.replace(/_/g, ' ')}</Checkbox>;
+        default:
+          return <Input />;
+      }
+    };
+
+    // Format the label
+    const formatLabel = (name: string) => {
+      const formatted = name.replace(/_/g, ' ');
+      return formatted.charAt(0).toUpperCase() + formatted.slice(1);
+    };
+
+    return (
+      <Col xs={24} sm={12} key={field.id}>
+        <Form.Item
+          className="font-semibold text-xs"
+          name={fieldName}
+          label={
+            <span className="mb-1 font-semibold text-xs">
+              {formatLabel(field.fieldName)}
+              {!field.isRequired && (
+                <span className="text-gray-400 font-normal ml-1">
+                  (Optional)
+                </span>
+              )}
+            </span>
+          }
+          rules={rules}
+          valuePropName={
+            field.fieldType === 'toggle' || field.fieldType === 'checkbox'
+              ? 'checked'
+              : 'value'
+          }
+        >
+          {renderInput()}
+        </Form.Item>
+      </Col>
+    );
   };
 
-  const renderRows = () => {
-    const rows = [];
-    for (let i = 0; i < fields?.length; i += 2) {
-      rows.push(
-        <Row gutter={16} key={`row-${i}`}>
-          <Col xs={24} sm={12}>
-            {fields[i] && renderField(fields[i])}
-          </Col>
-          <Col xs={24} sm={12}>
-            {fields[i + 1] && renderField(fields[i + 1])}
-          </Col>
-        </Row>,
-      );
-    }
-    return rows;
-  };
-
-  return <>{renderRows()}</>;
+  return <Row gutter={16}>{fields?.map((field) => renderField(field))}</Row>;
 };
 
 export default DynamicFormFields;
