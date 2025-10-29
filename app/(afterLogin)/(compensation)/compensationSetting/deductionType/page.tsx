92c6e20230a4df99f9f8c2d71e715e4da95c0417
dreambigatall <halidmahammed.16.hm@gmail.com>
2025-10-29 09:13:02 +0300

    updet

--- app/(afterLogin)/(compensation)/compensationSetting/deductionType/page.tsx
+++ app/(afterLogin)/(compensation)/compensationSetting/deductionType/page.tsx
@@ -10,7 +10,7 @@ import DeductiontypeSideBar from './_components/DeductiontypeSideBar';
 import { FaPlus } from 'react-icons/fa';
 
 const DeductionTypePage = () => {
-  const { setIsDeductionOpen } = useCompensationSettingStore();
+  const { setIsDeductionOpen, setSelectedDeductionRecord } = useCompensationSettingStore();
 
   return (
     <div className="bg-white rounded-lg px-1 py-4 sm:px-6">
@@ -23,6 +23,7 @@ const DeductionTypePage = () => {
             icon={<FaPlus />}
             className="h-10"
             onClick={() => {
+              setSelectedDeductionRecord(null);
               setIsDeductionOpen(true);
             }}
           >
