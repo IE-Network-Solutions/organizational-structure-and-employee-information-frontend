9d018f1d1715a7cce2b247dd3f16b039ee6dceed
nebyat19 <nebyat77bekele@yahoo.com>
2025-11-25 12:08:29 +0300

    PPII-1832 add id and data cy for each elements in compensation and benefit

--- app/(afterLogin)/(compensation)/compensationSetting/benefitType/page.tsx
+++ app/(afterLogin)/(compensation)/compensationSetting/benefitType/page.tsx
@@ -12,27 +12,54 @@ const BenefitTypePage = () => {
   const { setIsBenefitOpen } = useCompensationSettingStore();
 
   return (
-    <div className="bg-white rounded-lg px-1 py-4 sm:px-6">
-      <div className="flex justify-between mb-3 items-center">
-        <h1 className="text-lg font-bold">Benefit Types</h1>
-        <AccessGuard permissions={[Permissions.CreateBenefitType]}>
+    <div
+      className="bg-white rounded-lg px-1 py-4 sm:px-6"
+      id="compensation-settings-benefit-type-wrapper"
+      data-cy="compensation-settings-benefit-type-wrapper"
+    >
+      <div
+        className="flex justify-between mb-3 items-center"
+        id="compensation-settings-benefit-type-header"
+        data-cy="compensation-settings-benefit-type-header"
+      >
+        <h1
+          className="text-lg font-bold"
+          id="compensation-settings-benefit-type-title"
+          data-cy="compensation-settings-benefit-type-title"
+        >
+          Benefit Types
+        </h1>
+        <AccessGuard
+          permissions={[Permissions.CreateBenefitType]}
+          data-cy="compensation-settings-benefit-type-create-access-guard"
+        >
           <Button
             type="primary"
             id="createNewClosedHolidayFieldId"
-            icon={<FaPlus />}
+            icon={<FaPlus data-cy="compensation-settings-benefit-type-create-icon" />}
             className="h-10"
             onClick={() => {
               setIsBenefitOpen(true);
             }}
+            data-cy="compensation-settings-benefit-type-create-button"
           >
-            <span className="hidden lg:inline">Benefit</span>
+            <span
+              className="hidden lg:inline"
+              id="compensation-settings-benefit-type-create-button-text"
+              data-cy="compensation-settings-benefit-type-create-button-text"
+            >
+              Benefit
+            </span>
           </Button>
         </AccessGuard>
       </div>
-      <div>
-        <BenefitTypeTable />
+      <div
+        id="compensation-settings-benefit-type-table-wrapper"
+        data-cy="compensation-settings-benefit-type-table-wrapper"
+      >
+        <BenefitTypeTable data-cy="compensation-settings-benefit-type-table" />
       </div>
-      <BenefitypeSideBar />
+      <BenefitypeSideBar data-cy="compensation-settings-benefit-type-sidebar" />
     </div>
   );
 };
