847d0890776d7c33142dc98200d4637df5c2ef2e
Bini-tech27 <binilulseged@gmail.com>
2025-11-27 08:22:34 +0300

    add JobInfoAccessModal component

--- app/(afterLogin)/dashboard/_components/modal/index.tsx
+++ app/(afterLogin)/dashboard/_components/modal/index.tsx
@@ -11,6 +11,7 @@ const JobInfoAccessModal = ({
 }) => {
   return (
     <Modal
+      data-cy="job-info-access-modal"
       open={open}
       onCancel={onClose}
       footer={null}
@@ -23,14 +24,30 @@ const JobInfoAccessModal = ({
         borderRadius: 10,
       }}
     >
-      <h5 className="mb-10">
+      <h5
+        id="job-info-access-modal-title"
+        data-cy="job-info-access-modal-title"
+        className="mb-10"
+      >
         To access additional data, please enter your job information.
       </h5>
 
-      <div className="flex justify-center gap-4">
-        <Button onClick={onClose}>Cancel</Button>
+      <div
+        id="job-info-access-modal-buttons"
+        data-cy="job-info-access-modal-buttons"
+        className="flex justify-center gap-4"
+      >
+        <Button
+          id="job-info-access-modal-cancel-button"
+          data-cy="job-info-access-modal-cancel-button"
+          onClick={onClose}
+        >
+          Cancel
+        </Button>
 
         <Button
+          id="job-info-access-modal-confirm-button"
+          data-cy="job-info-access-modal-confirm-button"
           type="primary"
           style={{ padding: '0 28px' }}
           onClick={onConfirm}
