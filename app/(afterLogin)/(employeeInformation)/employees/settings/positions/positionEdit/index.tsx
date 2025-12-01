7b222733880ed0a154557710b1dbced71c41e16f
KibromHS <kibromhs81@gmail.com>
2025-12-01 10:08:19 +0300

    add missing attribute in employee module

--- app/(afterLogin)/(employeeInformation)/employees/settings/positions/positionEdit/index.tsx
+++ app/(afterLogin)/(employeeInformation)/employees/settings/positions/positionEdit/index.tsx
@@ -85,7 +85,7 @@ const PositionsEdit: React.FC = () => {
             data-cy="settings-position-edit-description-item"
             name="description"
             label={
-              <span className="text-md my-2 font-semibold text-gray-700">
+              <span className="text-md my-2 font-semibold text-gray-700" id="settings-position-edit-description-label" data-cy="settings-position-edit-description-label">
                 Position Description
               </span>
             }
