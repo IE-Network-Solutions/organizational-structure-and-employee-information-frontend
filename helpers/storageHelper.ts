ce5ab8fcdc8686b9a8e3f75ca49f85113ad40ebd
Abeselom Gebrekidan <abeselomgebrekidan12@gmail.com>
2025-12-03 09:17:31 +0300

    Remove unused js-cookie dependency from storageHelper to simplify cookie management

--- helpers/storageHelper.ts
+++ helpers/storageHelper.ts
@@ -1,5 +1,4 @@
 import { NextRequest } from 'next/server';
-import Cookies from 'js-cookie';
 /**
  * Stores a value in session storage under a given key
  * @param key The key under which the value is stored
@@ -64,5 +63,4 @@ export const getCookie = (key: string, request: NextRequest): string | null => {
 
 export const removeCookie = (key: string): void => {
   document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-  Cookies.remove(key, { path: '/' });
 };
