6ed4f9c0eaf7927af8698b7776fcdee6f53bc62b
dreambigatall <halidmahammed.16.hm@gmail.com>
2025-12-04 12:04:09 +0300

    lint issue fix

--- app/(afterLogin)/(tna)/tna/management/_components/courseSidebar/index.tsx
+++ app/(afterLogin)/(tna)/tna/management/_components/courseSidebar/index.tsx
@@ -472,14 +472,15 @@ const CourseCategorySidebar = () => {
       message.error('Select at least one employee to assign the course.');
       return;
     }
-    //eslint-disable-next-line @typescript-eslint/no-unused-vars
+    /* eslint-disable @typescript-eslint/no-unused-vars */
     const {
-      courseCategory: _courseCategory,
-      courseLessons: _courseLessons,
-      courseUsers: _courseUsers,
-      courseDepartments: _courseDepartments,
+      courseCategory,
+      courseLessons,
+      courseUsers,
+      courseDepartments,
       ...otherData
     } = coursesData?.items?.[0] ?? {};
+    /* eslint-enable @typescript-eslint/no-unused-vars */
     setCourse([
       {
         ...(otherData && otherData),
